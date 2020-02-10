a7294490501c66a24c3811a47bcf0498cd76e048
Renato Groffe <renato.groffe@yahoo.com.br>
2020-02-10 04:12:05 -0300

    Alteração no valor da propriedade Local

--- Controllers/ContadorController.cs
+++ Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste",
+                    Local = "GitHub Actions",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
