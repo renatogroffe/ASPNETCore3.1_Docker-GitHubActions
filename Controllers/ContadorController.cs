b8d34eea99a4183252709e1a5dd264a5805b8b3f
Renato Groffe <renato.groffe@yahoo.com.br>
2020-02-16 23:44:05 -0300

    Nova alteração no valor da propriedade Local

--- Controllers/ContadorController.cs
+++ Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "GitHub Actions",
+                    Local = "GitHub Actions + Azure Web App for Containers",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
